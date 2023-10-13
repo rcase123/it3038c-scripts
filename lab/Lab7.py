59a2744a8281b3cdfdfb54a3cc450eb32b5d0098
rcase123 <143114785+rcase123@users.noreply.github.com>
2023-10-13 01:18:01 -0400

    Add files via upload
    the plug in is:  pip install requests

---
+++ lab/Lab7.py
@@ -0,0 +1,12 @@
+﻿import requests
+from pprint import pprint
+
+API = 'abb83b4e43fae2414a7534cc16e5d683'
+
+City= input("Enter your city: ")
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + API +"&q="+City
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data)
