c9b3b785466b5e9bcfecbf7608bfb3849fd2a0f8
rcase123 <143114785+rcase123@users.noreply.github.com>
2023-10-14 16:09:59 -0400

    Update Lab7.py
    Weather application Lab

--- lab/Lab7.py
+++ lab/Lab7.py
@@ -1,12 +1,27 @@
 ﻿import requests
+
 from pprint import pprint
+
+#importing the plug ins
 
 API = 'abb83b4e43fae2414a7534cc16e5d683'
 
-City= input("Enter your city: ")
+#This is my personal API from openweather.com.
 
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + API +"&q="+City
+City = raw_input("Enter your city: ")
+
+#This is the input for the city to search for the weather.
+#The website has limitations in searching, but you can be more specific by adding ,US after.
+#Either one works.
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + API + "&q=" + City
+
+#This is piecing together the input with a url, api and the input.
 
 weather_data = requests.get(base_url).json()
 
+#This uses the request plug in and the baseurl variable to pull the information from the site.
+
 pprint(weather_data)
+
+#This prints all data involved
